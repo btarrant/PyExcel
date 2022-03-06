83839ed2321ecc5804b0655bdb4aa7d8868c66e2
Brandon Tarrant <brandontarrant04@gmail.com>
2022-03-05 22:42:01 -0500

    For loop that prints all values in the spreadsheet.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ print(wb.sheetnames)
 sheet = wb["Sheet1"]
 
 cell = sheet["a1"]
-print(cell.row)
-print(cell.column)
-print(cell.coordinate)
+print(sheet.max_row)
+print(sheet.max_column)
+
+for row in range(1, sheet.max_row + 1):
+    for column in range(1, sheet.max_column + 1):
+        cell = sheet.cell(row, column)
+        print(cell.value)
