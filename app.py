0100728ea48501c575cf02f619a42f1359255962
Brandon Tarrant <brandontarrant04@gmail.com>
2022-03-05 22:35:35 -0500

    Pulled data from Sheet 1.

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import openpyxl
 # wb = openpyxl.Workbook()
 wb = openpyxl.load_workbook("transactions.xlsx")
 print(wb.sheetnames)
+
+sheet = wb["Sheet1"]
+
+cell = sheet["a1"]
+print(cell.row)
+print(cell.column)
+print(cell.coordinate)
